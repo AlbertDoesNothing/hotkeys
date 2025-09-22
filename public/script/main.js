8eac86312fd2ded1cd88dbcd785e3d050659a5a4
λlbert <72ports@gmail.com>
2025-09-22 17:00:26 +0700

    Fixing bugs, rewriting code and optimizing

--- public/script/main.js
+++ public/script/main.js
@@ -1,13 +1,13 @@
 // helper to call the exposed Python API
 async function callPy(method, ...args) {
-  if (window.pywebview) {
-    try {
-      const res = await window.pywebview.api[method](...args);
-      return res;
-    } catch (e) {
-      console.error("pywebview API error:", e);
+    if (window.pywebview) {
+        try {
+        const res = await window.pywebview.api[method](...args);
+        return res;
+        } catch (e) {
+        console.error("pywebview API error:", e);
+        }
     }
-  }
 }
 
 // minimize button
